b0d4587e63f1ef7855647f3a43605baf8a243b5d
Kofemolka <kubovich@gmail.com>
2019-03-04 20:39:12 +0200

    Range selection added for plot.py

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,15 +1,29 @@
 from csv import reader
 from matplotlib import pyplot
+import numpy as np
 import sys
 
 with open(sys.argv[1], 'r') as f:
     data = list(reader(f))
 
+    Tbegin = 1
+    Tend = len(data)
+
+    if len(sys.argv) >= 3:
+         Tbegin = np.searchsorted([row[0] for row in data[1:]], int(sys.argv[2]))
+    if Tbegin == 0:
+         Tbegin = 1
+
+    if len(sys.argv) >= 4:
+         Tend = np.searchsorted([row[0] for row in data[1:]], int(sys.argv[3]))
+
+    print "T=(" + str(Tbegin) + "-" + str(Tend) + ")"
+
     #startTime = int(data[1][0])
-    T = [(int(i[0]))/1000 for i in data[1:]]    # seconds
-    P = [float(i[4])*1000 for i in data[1:]]    # mW
-    I = [float(i[3])*1000 for i in data[1:]]    # mA
-    Ph = [float(i[5])*1000 for i in data[1:]]   # mWms
+    T = [(int(i[0]))/1000 for i in data[Tbegin:Tend]]    # seconds
+    P = [float(i[4])*1000 for i in data[Tbegin:Tend]]    # mW
+    I = [float(i[3])*1000 for i in data[Tbegin:Tend]]    # mA
+    Ph = [float(i[5])*1000 for i in data[Tbegin:Tend]]   # mWms
 
     Imax = max(I)
     Tall = T[-1] - T[0]                         # seconds
